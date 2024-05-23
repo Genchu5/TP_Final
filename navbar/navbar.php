ec6eeb8c0b3873ade2a6f9b2d62bc19c7158f144
Your Name <you@example.com>
2024-05-22 22:13:49 -0300

    navbar

---
+++ navbar/navbar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+        
+        <link rel="stylesheet" type="text/css" href="http://localhost/pruebas_entornos/navbar/style_navbar.css">
+
+        <title>D&B Shopping</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="http://localhost/pruebas_entornos/img/logo1_transparente.png" class="logo"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav me-auto">
+                        <a class="animated-btn abajo" href="http://localhost/pruebas_entornos/home.php">Home</a>
+                        <a class="animated-btn abajo" href="#">Locales</a>
+                        <a class="animated-btn abajo" href="#">Promos</a>
+                        <a class="animated-btn abajo" href="#">Novedades</a>
+                        <a class="animated-btn abajo" href="#">Contacto</a>
+                    </div>
+                    <div class="navbar-nav ms-auto">
+                        <a class="animated-btn abajo" href="http://localhost/pruebas_entornos/login_register/login_register.php">Log in</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </body>
+</html>
